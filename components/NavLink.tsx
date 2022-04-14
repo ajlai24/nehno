b923c0cf47bc13c10c2d54b3b7f0051c85d4fd6c
ajlai24 <ajlai24@gmail.com>
2022-04-14 11:34:25 -0700

    Animated nav link

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -2,72 +2,34 @@ import { Link as MuiLink, styled } from '@mui/material';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-// const lineStyle = {
-//   //   backface-visibility: hidden;
-//   border: `1px solid green`,
-//   // bottom: 0,
-//   content: "''",
-//   display: 'block',
-//   //   margin: 0 auto;
-//   //   position: relative;
-//   //   transition: all 280ms ease-in-out;
-//   width: 0,
-// };
-
 const StyledLink = styled(MuiLink)(({ theme }) => ({
-  // color: #fff;
-  // cursor: pointer;
-  // // display: block;
-  // font-size:16px;
-  // font-weight: 400;
-  // line-height: 45px;
-  // margin: 0 0 2em;
-  // max-width: 160px; 
-  // position: relative;
-  // text-decoration: none;
-  // text-transform: uppercase;
-  // width: 100%; 
-  transition: theme.transitions.create(['color'], { duration: theme.transitions.duration.complex }),
+  display: 'block',
+  padding: theme.spacing(1),
+  textDecoration: 'none',
+  color: theme.palette.primary.light,
+  fontWeight: theme.typography.fontWeightBold,
+  textTransform: 'uppercase',
+  margin: theme.spacing(0, 1),
+  position: 'relative',
+  ':after': {
+    transition: theme.transitions.create(['all'], { duration: theme.transitions.duration.complex }),
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: 'auto',
+    width: 0,
+    content: "'.'",
+    color: 'transparent',
+    background: theme.palette.primary.dark,
+    height: '1px',
+  },
   ':hover': {
     color: theme.palette.primary.dark,
-    // ':before': {
-    //   bottom: 'auto',
-    //   top: 0,
-    //   width: '70 %',
-    // },
-    // ':after': {
-    //   width: '70 %',
-    // },
+    ':after': {
+      width: '100%',
+    },
   },
-  // ':before': lineStyle,
-  // ':after': lineStyle,
-
-  // .btn-2:after,
-  // .btn-2:before {
-  //   backface-visibility: hidden;
-  //   border: 1px solid rgba(#fff, 0);
-  //   bottom: 0px;
-  //   content: " ";
-  //   display: block;
-  //   margin: 0 auto;
-  //   position: relative;
-  //   transition: all 280ms ease-in-out;
-  //   width: 0;
-  // }
-
-  // .btn-2:hover:after,
-  // .btn-2:hover:before {
-  //   backface-visibility: hidden;
-  //   border-color: #fff;
-  //   transition: width 350ms ease-in-out;
-  //   width: 70%;
-  // }
-
-  // .btn-2:hover:before {
-  //   bottom: auto;
-  //   top: 0;
-  //   width: 70%;
-  // }
 }))
 
 interface NavLinkProps {
